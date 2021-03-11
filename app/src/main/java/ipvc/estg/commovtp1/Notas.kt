913d014bb427de7d630984382a64d17ae6866e67
Vaultboy27 <77488836+Vaultboy27@users.noreply.github.com>
2021-03-11 18:59:28 +0000

    notas-layout

---
+++ app/src/main/java/ipvc/estg/commovtp1/Notas.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.commovtp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notas)
+    }
+}
