8de68cf8964e9b070d900c463986502fb674af04
Vaultboy27 <77488836+Vaultboy27@users.noreply.github.com>
2021-04-10 22:16:53 +0100

    Login com sucesso

---
+++ app/src/main/java/ipvc/estg/commovtp1/API/Endpoints.kt
@@ -0,0 +1,15 @@
+package ipvc.estg.commovtp.API
+
+
+import retrofit2.http.*
+import retrofit2.http.Path
+import retrofit2.Call
+
+interface Endpoints {
+
+    @GET("/myslim/API/user")
+    fun  getUsers(): Call<List<User>>
+
+    @GET("/user/{id}")
+    fun getUserById(@Path("id") id:Int): Call<User>
+}
