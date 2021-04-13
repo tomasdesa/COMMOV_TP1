2829a0f4056af9139f4dc54efe58df91f6cf328a
Vaultboy27 <77488836+Vaultboy27@users.noreply.github.com>
2021-04-13 17:58:01 +0100

    Login com splash screen

---
+++ app/src/main/java/ipvc/estg/commovtp1/Splash.kt
@@ -0,0 +1,33 @@
+package ipvc.estg.commovtp1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import com.airbnb.lottie.LottieAnimationView
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val splash =findViewById<LottieAnimationView>(R.id.splash1)
+        val fundo =findViewById<ImageView>(R.id.fundo)
+        val fundbranco =findViewById<ImageView>(R.id.fundbranco)
+
+        splash.setMaxFrame(300)
+        splash.playAnimation()
+
+
+        splash.animate().translationY((-1600).toFloat()).setDuration(1000).setStartDelay(4000)
+        fundo.animate().translationY((-1800).toFloat()).setDuration(1000).setStartDelay(4000)
+        fundbranco.animate().translationY((-1600).toFloat()).setDuration(1000).setStartDelay(4000)
+
+        Handler().postDelayed({
+            val i = Intent(this, Login::class.java)
+            startActivity(i)
+        }, 5000)
+    }
+
+}
